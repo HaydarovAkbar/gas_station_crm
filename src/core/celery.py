83779fbde320fd8762b78c3115edd1d7cfcb97fd
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2024-02-15 14:17:09 +0500

    change celery.py

--- src/core/celery.py
+++ src/core/celery.py
@@ -8,5 +8,17 @@ app = Celery("core")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
+# app.conf.enable_utc = False
+# app.conf.timezone = "Asia/Tashkent"
+
 app.conf.beat_schedule = {
+    "check_all_users_result": {
+        "task": "bot.tasks.check_all_users_result",
+        "schedule": 10.0,
+        # 'every': 10.0,
 }
+
+
+# @app.task(bind=True)
+# def debug_task(self):
+#     print(f"Request: {self.request!r}")
