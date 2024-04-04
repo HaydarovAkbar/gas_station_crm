af47bc7b147bded768baa5f91216c002351a6878
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2024-04-04 09:52:13 +0500

    change code ...

--- src/methods/core/report.py
+++ src/methods/core/report.py
@@ -42,10 +42,40 @@ def get_report_1(user, week=False, fuel_type=None):
         ws.merge_cells(f'C{i}:C{i + organ_fuel_columns - 1}')
         ws[f'C{i}'] = residual
         ws.merge_cells(f'D{i}:D{i + organ_fuel_columns - 1}')
-        ws[f'D{i}'] = FuelStorage.objects.filter(organization=user.organization, fuel_type=fuel_type, created_at__lt=date).last().remainder
+        ws[f'D{i}'] = FuelStorage.objects.filter(organization=user.organization, fuel_type=fuel_type,
+                                                 created_at__lt=date).last().remainder
         pointer = FuelColumnPointer.objects.filter(organization=user.organization, fuel_type=fuel_type,
-                                                       created_at__lt=date)
-        ws[f'E{i}'] = pointer.size_first
+                                                   created_at__lt=date)
+        item = 0
+        for point in pointer:
+            ws[f'E{i + item}'] = point.size_last
+            ws[f'F{i + item}'] = point.size_first
+        sale_fuel = SaleFuel.objects.filter(organization=user.organization, fuel_type=fuel_type, created_at__lt=date)
+        fuel_price_last = FuelPrice.objects.filter(organization=user.organization, fuel_type=fuel_type, created_at__lt=date).last()
+        ws.merge_cells(f'G{i}:G{i + organ_fuel_columns - 1}')
+        ws[f'G{i}'] = sale_fuel.aggregate(total=Sum('cash_size'))['total'] + sale_fuel.aggregate(
+            total=Sum('card_size'))['total']
+        ws.merge_cells(f'H{i}:H{i + organ_fuel_columns - 1}')
+        ws[f'H{i}'] = sale_fuel.first().price - sale_fuel.first().benefit
+        ws.merge_cells(f'I{i}:I{i + organ_fuel_columns - 1}')
+        ws[f'I{i}'] = fuel_price_last.price
+        ws.merge_cells(f'J{i}:J{i + organ_fuel_columns - 1}')
+        ws[f'J{i}'] = sale_fuel.first().price
+        ws.merge_cells(f'K{i}:K{i + organ_fuel_columns - 1}')
+        ws[f'K{i}'] = sale_fuel.first().card_size
+        ws.merge_cells(f'L{i}:L{i + organ_fuel_columns - 1}')
+        ws[f'L{i}'] = sale_fuel.first().card_size * fuel_price_last.price
+        ws.merge_cells(f'M{i}:M{i + organ_fuel_columns - 1}')
+        ws[f'M{i}'] = sale_fuel.first().cash_size
+        ws.merge_cells(f'N{i}:N{i + organ_fuel_columns - 1}')
+        ws[f'N{i}'] = sale_fuel.first().cash_size * fuel_price_last.price
+        ws.merge_cells(f'O{i}:O{i + organ_fuel_columns - 1}')
+        ws[f'O{i}'] = sale_fuel.first().benefit
+        ws.merge_cells(f'P{i}:P{i + organ_fuel_columns - 1}')
+        ws[f'P{i}'] = 6878
+
+
+
 
 
     wb.save("test.xlsx")
