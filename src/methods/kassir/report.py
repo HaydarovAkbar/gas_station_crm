638a7b25b093dc719814348ed1bb59296de1e670
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2024-04-07 20:12:08 +0500

    add pdf generator to project

---
+++ src/methods/kassir/report.py
@@ -0,0 +1,12 @@
+from fpdf import FPDF
+
+
+def generate_pdf():
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font('Arial', size=12)
+    pdf.cell(200, 10, 'Hello World!', 0, 1, 'C')
+    pdf.output('tuto1.pdf')
+
+
+generate_pdf()
