cd691e86bf989b52b5918bc24204e0130135ae57
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2024-04-09 18:19:19 +0500

    fg

--- src/methods/kassir/report.py
+++ src/methods/kassir/report.py
@@ -8,7 +8,8 @@ def generate_pdf():
     pdf.add_page()
     pdf.set_font('Arial', size=12)
     pdf.cell(200, 10, f"Bugungi hisobot", 0, 1, 'C')
-    pdf.cell(200, 10, f"Hisobot yaratilgan vaqt: {timezone.now().strftime('%Y.%m.%d %H:%M')}", 0, 1, 'C')
+    pdf.cell(200, 200, f"Hisobot yaratilgan vaqt: {timezone.now().strftime('%Y.%m.%d %H:%M')}", 0, 1, 'C')
+    pdf.cell(200, 250, f"Hisobot turi: Kunlik", 0, 1, 'C')
     pdf.output(path)
     return path
 
